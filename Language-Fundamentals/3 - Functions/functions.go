74d493a58c3b864c34182a8d5f92b6bf40a7ee8a
declau <declaudiano89@gmail.com>
2025-03-29 20:40:44 -0300

    function with multiple returns

--- Language-Fundamentals/3 - Functions/functions.go
+++ Language-Fundamentals/3 - Functions/functions.go
@@ -7,7 +7,26 @@ func add(n1 int8, n2 int8) int8 {
 
 }
 
+func mathematicalCalculations(n1, n2 int8) (int8, int8) {
+	add := n1 + n2
+	subtraction := n1 - n2
+	return add, subtraction
+
+}
+
 func main() {
 	adding := add(10, 20)
 	fmt.Println(adding)
+
+	var f = func(txt string) {
+		fmt.Println(txt)
+	}
+
+	f("Text fron function f")
+
+	addResult, subtractionresult := mathematicalCalculations(10, 20)
+	fmt.Println(addResult, subtractionresult)
+
+	addResult2, _ := mathematicalCalculations(10, 20)
+	fmt.Println(addResult2)
 }
