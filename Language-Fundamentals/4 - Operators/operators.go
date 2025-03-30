9ce283f3e75d06c74296b018180a03e8a0c88a09
declau <declaudiano89@gmail.com>
2025-03-30 16:50:09 -0300

    operators

---
+++ Language-Fundamentals/4 - Operators/operators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arithmetic
+	addtion := 1 + 1
+	subtraction := 2 - 1
+	multiplication := 2 * 2
+	division := 10 / 2
+	restDivision := 10 % 2
+
+	fmt.Println(addtion, subtraction, multiplication, division, restDivision)
+}
