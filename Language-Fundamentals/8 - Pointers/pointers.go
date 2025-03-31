0c9ac6ed301edc67602fac16f217c27cebb1e557
declau <declaudiano89@gmail.com>
2025-03-31 14:10:42 -0300

    pointers adjusts

--- Language-Fundamentals/8 - Pointers/pointers.go
+++ Language-Fundamentals/8 - Pointers/pointers.go
@@ -12,4 +12,15 @@ func main() {
 
 	var1++
 	fmt.Println(var1, var2)
+
+	// Pointers
+	var var3 int
+	var pointer *int
+
+	var3 = 100
+	pointer = &var3
+	fmt.Println(var3, *pointer) //The * is a dereferencing process
+
+	var3 = 150
+	fmt.Println(var3, *pointer) //The * is a dereferencing process
 }
