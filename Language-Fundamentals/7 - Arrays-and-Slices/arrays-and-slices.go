ec754380bb5d901e3bfc1175f7605484a716f396
declau <declaudiano89@gmail.com>
2025-03-31 12:02:40 -0300

    slices

--- Language-Fundamentals/7 - Arrays-and-Slices/arrays-and-slices.go
+++ Language-Fundamentals/7 - Arrays-and-Slices/arrays-and-slices.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	fmt.Println("Arrays and Slices")
@@ -14,6 +17,24 @@ func main() {
 	fmt.Println(array1)
 
 	array2 := [5]string{"position 1", "position 2", "position 3", "position 4", "position 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{1, 2, 3, 10, 11, 12, 13}
+	fmt.Println(slice)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+
+	slice = append(slice, 14)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Alter Position"
 	fmt.Println(array2)
 
 }
