df1c87d34143c15f3ff2d9c72aa96fd7ab050b80
declau <declaudiano89@gmail.com>
2025-04-02 18:58:45 -0300

    user

--- 3 - Databases/CRUD-Basic/server/server.go
+++ 3 - Databases/CRUD-Basic/server/server.go
@@ -1,7 +1,29 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
+type user struct {
+	ID    uint32 `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+}
+
+// Create a user in DB
 func CreatUser(w http.ResponseWriter, r *http.Request) {
+	requestBody, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Failed to read request body"))
+		return
+	}
 
+	var user user
+
+	if erro = json.Unmarshal(requestBody, &user); erro != nil {
+		w.Write([]byte("Erro t convert user to struct"))
+		return
+	}
 }
