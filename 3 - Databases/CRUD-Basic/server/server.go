7b3cebad9fdaacccf496c9857e274a7436664cb2
declau <declaudiano89@gmail.com>
2025-04-02 19:26:34 -0300

    id insert

--- 3 - Databases/CRUD-Basic/server/server.go
+++ 3 - Databases/CRUD-Basic/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -26,4 +27,33 @@ func CreatUser(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Erro t convert user to struct"))
 		return
 	}
+
+	db, erro := db.connect()
+	if erro != nil {
+		w.Write([]byte("DB connection error"))
+		return
+	}
+
+	defer db.Close()
+
+	statement, erro := db.Prepare("insert into users (nome, email) values (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Statement creator error"))
+		return
+	}
+	defer statement.Close()
+
+	insert, erro := statement.Exec(user.Nome, user.Email)
+	if erro != nil {
+		w.Write([]byte("Statement execution error"))
+		return
+	}
+
+	idInsert, erro := insert.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("ID insert error"))
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("insert user ok! ID: %d", idInsert)))
 }
