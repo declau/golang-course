ebbd56fd5c65df94d29c6b929772ccd5aa58186d
declau <declaudiano89@gmail.com>
2025-04-02 18:45:59 -0300

    POST

--- 3 - Databases/CRUD-Basic/main.go
+++ 3 - Databases/CRUD-Basic/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/server"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ func main() {
 	fmt.Println("CRUD")
 
 	router := mux.NewRouter()
+	router.HandleFunc("/users", server.CreatUser).Methods(http.MethodPost)
 
 	fmt.Println("Listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
---
+++ 3 - Databases/CRUD-Basic/server/server.go
@@ -0,0 +1,7 @@
+package server
+
+import "net/http"
+
+func CreatUser(w http.ResponseWriter, r *http.Request) {
+
+}
