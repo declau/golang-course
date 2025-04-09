98cd2141dfa373ee9f73e77d76e5ddfcf5c01267
declau <declaudiano89@gmail.com>
2025-04-09 12:03:48 -0300

    func pointers adjusts

--- 2 - Language-Fundamentals/13 - Advanced Functions/13.8 - Functions With Pointers/pointer-functions.go
+++ 2 - Language-Fundamentals/13 - Advanced Functions/13.8 - Functions With Pointers/pointer-functions.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+// Without Pointer
 func signalInverter(number int) int {
 	return number * -1
 }
 
+// With Pointer
+func signalInverterWithPointer(number *int) {
+	*number = *number * -1
+}
+
 func main() {
 	fmt.Println("Pointer Functions!")
+
 	number := 20
 	inverterNumber := signalInverter(number)
 	fmt.Println(inverterNumber)
 	fmt.Println(number)
+
+	newNumber := 40
+	fmt.Println(newNumber)
+	signalInverterWithPointer(&newNumber)
+	fmt.Println(newNumber)
 }
