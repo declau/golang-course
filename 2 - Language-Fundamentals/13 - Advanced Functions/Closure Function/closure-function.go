9808df8ce1cebd62803e015d6b171601a1d53ae0
declau <declaudiano89@gmail.com>
2025-04-09 11:44:22 -0300

    closure

--- 2 - Language-Fundamentals/13 - Advanced Functions/Closure Function/closure-function.go
+++ 2 - Language-Fundamentals/13 - Advanced Functions/Closure Function/closure-function.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func closure() func() {
+	text := "Inside the closure function"
+
+	function := func() {
+		fmt.Println(text)
+	}
+
+	return function
+}
+
+func main() {
+	text := "Inside the main function"
+	fmt.Println(text)
+
+	newFunction := closure()
+	newFunction()
 }
