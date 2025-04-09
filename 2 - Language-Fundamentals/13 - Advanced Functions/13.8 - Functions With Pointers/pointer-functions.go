8baace6c95dbcec7baf14a8fb2a306a6907dc1cb
declau <declaudiano89@gmail.com>
2025-04-09 11:57:07 -0300

    adjusts

--- 2 - Language-Fundamentals/13 - Advanced Functions/13.8 - Functions With Pointers/pointer-functions.go
+++ 2 - Language-Fundamentals/13 - Advanced Functions/13.8 - Functions With Pointers/pointer-functions.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func signalInverter(number int) int {
+	return number * -1
+}
+
 func main() {
 	fmt.Println("Pointer Functions!")
+	number := 20
+	inverterNumber := signalInverter(number)
+	fmt.Println(inverterNumber)
+	fmt.Println(number)
 }
