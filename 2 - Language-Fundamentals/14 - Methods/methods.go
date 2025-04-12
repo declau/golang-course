bcc3d24b3031a73488e1a5ff5b7224e7b2cfeec8
declau <declaudiano89@gmail.com>
2025-04-12 16:26:40 -0300

    of legal age

--- 2 - Language-Fundamentals/14 - Methods/methods.go
+++ 2 - Language-Fundamentals/14 - Methods/methods.go
@@ -11,13 +11,20 @@ func (u user) save() {
 	fmt.Printf("Save data from user %s on database\n", u.name)
 }
 
+func (u user) majority() bool {
+	return u.age >= 18
+}
+
 func main() {
 
 	user1 := user{"User 1", 33}
 	fmt.Println(user1)
 	user1.save()
 
-	user2 := user{"Dec", 42}
+	user2 := user{"Dec", 17}
 	user2.save()
+
+	majority := user2.majority()
+	fmt.Println(majority)
 
 }
