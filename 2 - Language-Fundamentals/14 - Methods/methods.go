7c6dcace783b6d8c3defc76b6809b66042192fac
declau <declaudiano89@gmail.com>
2025-04-12 16:19:40 -0300

    methods

--- 2 - Language-Fundamentals/14 - Methods/methods.go
+++ 2 - Language-Fundamentals/14 - Methods/methods.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
+type user struct {
+	name string
+	age  uint8
+}
+
+func (u user) save() {
+	fmt.Printf("Save data from user %s on database", u.name)
+}
+
 func main() {
+
+	user1 := user{"User 1", 33}
+	fmt.Println(user1)
+	user1.save()
 
 }
