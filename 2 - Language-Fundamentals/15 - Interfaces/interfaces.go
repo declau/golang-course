a080d408f0ad6fc05292bde3a0d94f8629c451ad
declau <declaudiano89@gmail.com>
2025-04-14 20:05:31 -0300

    adjusts

--- 2 - Language-Fundamentals/15 - Interfaces/interfaces.go
+++ 2 - Language-Fundamentals/15 - Interfaces/interfaces.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type form interface {
 	area() float64
@@ -23,8 +26,16 @@ type circle struct {
 	radius float64
 }
 
+func (c circle) area() float64 {
+	// return math.Pi * (c.radius * c.radius)
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
 func main() {
 	fmt.Println("Interfaces")
 	r := rectangle{10, 15}
 	writeArea(r)
+
+	c := circle{10}
+	writeArea(c)
 }
