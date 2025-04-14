c2bdd0a4f73081a515ee965837195c1818aec283
declau <declaudiano89@gmail.com>
2025-04-14 19:52:59 -0300

    add interface

--- 2 - Language-Fundamentals/14 - Methods/15 - Interfaces/interfaces.go
+++ 2 - Language-Fundamentals/14 - Methods/15 - Interfaces/interfaces.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type rectangle struct {
+	height float64
+	width  float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type form interface {
+	area() float64
+}
+
 func main() {
 	fmt.Println("Interfaces")
 }
