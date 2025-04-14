cff2abc3b40cf905ab4317d557971349af42b745
declau <declaudiano89@gmail.com>
2025-04-14 19:59:12 -0300

    func rectangle

--- 2 - Language-Fundamentals/14 - Methods/15 - Interfaces/interfaces.go
+++ 2 - Language-Fundamentals/14 - Methods/15 - Interfaces/interfaces.go
@@ -2,19 +2,29 @@ package main
 
 import "fmt"
 
+type form interface {
+	area() float64
+}
+
+func writeArea(f form) {
+	fmt.Printf(" The form area is %0.2f\n", f.area())
+}
+
 type rectangle struct {
 	height float64
 	width  float64
+}
+
+func (r rectangle) area() float64 {
+	return r.height * r.width
 }
 
 type circle struct {
 	radius float64
 }
 
-type form interface {
-	area() float64
-}
-
 func main() {
 	fmt.Println("Interfaces")
+	r := rectangle{10, 15}
+	writeArea(r)
 }
